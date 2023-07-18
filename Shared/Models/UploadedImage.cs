e05685be0c62dec8bb5908f94941607feb795c0d
astralwaters <markobattlenet@gmail.com>
2023-07-18 14:30:06 +0200

    Added image uploading

---
+++ Shared/Models/UploadedImage.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Models
+{
+	public class UploadedImage
+	{
+        public string NewImageFileExtension { get; set; }
+        //base64  string represents binary
+        public string NewImageBase64Content { get; set; }
+        public string OldImagePath { get; set; }
+    }
+}
